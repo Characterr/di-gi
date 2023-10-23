b3168ae59bdf756cb82c15618a6b3404423ef1cb
YuriySoroka <40ayuriy@gmail.com>
2023-10-23 04:43:49 +0300

    Add slider and edits

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-$(function(){
-
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        nav: true,
+        loop: true,
+        checkVisible: false,
+        startPosition: 1,
+    });
 });
